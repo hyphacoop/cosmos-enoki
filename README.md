b95ae46eb7f498bd9033148b144b9913af015b23
Dante Sanchez <dante.a.sanchez@gmail.com>
2025-04-24 09:16:13 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# cosmos-enoki
-Reference Interchain Build
+# Generated With [Spawn](https://github.com/rollchains/spawn)
+
+## Testnet
+
+- `make testnet` *IBC testnet from chain <-> local cosmos-hub*
+- `make sh-testnet` *Single node, no IBC. quick iteration*
+- `local-ic chains` *See available testnets from the chains/ directory*
+- `local-ic start <name>` *Starts a local chain with the given name*
+
+## Local Images
+
+- `make install`      *Builds the chain's binary*
+- `make local-image`  *Builds the chain's docker image*
+
+## Testing
+
+- `go test ./... -v` *Unit test*
+- `make ictest-*`  *E2E testing*
+
---
+++ app/ante/ante_cosmos.go
@@ -0,0 +1,35 @@
+package ante
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth/ante"
+
+	circuitante "cosmossdk.io/x/circuit/ante"
+	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
+	ibcante "github.com/cosmos/ibc-go/v8/modules/core/ante"
+)
+
+// newCosmosAnteHandler creates the default ante handler for Cosmos transactions
+func NewCosmosAnteHandler(options HandlerOptions) sdk.AnteHandler {
+
+	return sdk.ChainAnteDecorators(
+		ante.NewSetUpContextDecorator(),
+		wasmkeeper.NewLimitSimulationGasDecorator(options.WasmConfig.SimulationGasLimit), // after setup context to enforce limits early
+		wasmkeeper.NewCountTXDecorator(options.TXCounterStoreService),
+		wasmkeeper.NewGasRegisterDecorator(options.WasmKeeper.GetGasRegister()),
+		circuitante.NewCircuitBreakerDecorator(options.CircuitKeeper),
+		ante.NewExtensionOptionsDecorator(options.ExtensionOptionChecker),
+		ante.NewValidateBasicDecorator(),
+		ante.NewTxTimeoutHeightDecorator(),
+		ante.NewValidateMemoDecorator(options.AccountKeeper),
+		ante.NewConsumeGasForTxSizeDecorator(options.AccountKeeper),
+		ante.NewDeductFeeDecorator(options.AccountKeeper, options.BankKeeper, options.FeegrantKeeper, options.TxFeeChecker),
+		// SetPubKeyDecorator must be called before all signature verification decorators
+		ante.NewSetPubKeyDecorator(options.AccountKeeper),
+		ante.NewValidateSigCountDecorator(options.AccountKeeper),
+		ante.NewSigGasConsumeDecorator(options.AccountKeeper, options.SigGasConsumer),
+		ante.NewSigVerificationDecorator(options.AccountKeeper, options.SignModeHandler),
+		ante.NewIncrementSequenceDecorator(options.AccountKeeper),
+		ibcante.NewRedundantRelayDecorator(options.IBCKeeper),
+	)
+}
