a4cae718ab64186b29a651fabad5b727f05cca8d
Dante Sanchez <dante.a.sanchez@gmail.com>
2026-04-22 11:15:03 -0400

    upgrade to main

--- interchaintest/main_test.go
+++ interchaintest/main_test.go
@@ -7,9 +7,9 @@ import (
 )
 
 var (
-	flagPreUpgradeVersion  = flag.String("pre-upgrade-version", "v1.8.0", "Docker image version to use before the upgrade")
-	flagPostUpgradeVersion = flag.String("post-upgrade-version", "v1.9.0", "Docker image version to use after the upgrade")
-	flagUpgradeName        = flag.String("upgrade-name", "v1.9.0", "On-chain upgrade name used in the governance proposal")
+	flagPreUpgradeVersion  = flag.String("pre-upgrade-version", "v2.1.0", "Docker image version to use before the upgrade")
+	flagPostUpgradeVersion = flag.String("post-upgrade-version", "main", "Docker image version to use after the upgrade")
+	flagUpgradeName        = flag.String("upgrade-name", "v3.0.0", "On-chain upgrade name used in the governance proposal")
 )
 
 func TestMain(m *testing.M) {
